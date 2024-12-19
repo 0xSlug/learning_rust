665bd5643b7350c153620286aa6edf00bc5d547e
slug <slug@slug.com>
2024-12-18 19:28:37 -0500

    finished chapter 1

---
+++ hello_cargo/target/debug/deps/hello_cargo-ecd8def3cace2029.d
@@ -0,0 +1,5 @@
+/home/slug/code/learning_rust/hello_cargo/target/debug/deps/libhello_cargo-ecd8def3cace2029.rmeta: src/main.rs
+
+/home/slug/code/learning_rust/hello_cargo/target/debug/deps/hello_cargo-ecd8def3cace2029.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/slug/code/learning_rust/hello_cargo/target/debug/hello_cargo: /home/slug/code/learning_rust/hello_cargo/src/main.rs

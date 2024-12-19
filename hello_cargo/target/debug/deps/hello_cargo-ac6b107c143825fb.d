a27f3d28a833c43221a3e48ad328d40d08a45bc4
slug <slug@slug.com>
2024-12-18 19:33:40 -0500

    fix .gitignore, remove target dir

--- hello_cargo/target/debug/deps/hello_cargo-ac6b107c143825fb.d
+++
@@ -1,5 +0,0 @@
-/home/slug/code/learning_rust/hello_cargo/target/debug/deps/hello_cargo-ac6b107c143825fb: src/main.rs
-
-/home/slug/code/learning_rust/hello_cargo/target/debug/deps/hello_cargo-ac6b107c143825fb.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/slug/code/learning_rust/hello_cargo/target/debug/hello_cargo: /home/slug/code/learning_rust/hello_cargo/src/main.rs
